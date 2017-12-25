224632096138867077517e9642cb837907d54850
pbache <34801407+pbache@users.noreply.github.com>
2017-12-25 14:40:24 -0800

    Update Common.groovy

--- Common.groovy
+++ Common.groovy
@@ -1,1 +1,13 @@
+def name = sh ( script: 'git show --name-only origin/master' , returnStdout:true).trim()/split('/')
+println("----------output-----------")
+for (i = 0; i < name.length; i++)
+  printlln(name[i])
+if(name[i] == 'ui-web'){
+  def pipeui=load 'pipeline/ui-web.groovy'
+  pipeui.testui()
+}
+else if(name[i] == 'lambdas'){
+  def pipeserv=load 'pipeline/ui-web.groovy'
+  pipeserv.testservices()
+}
 
