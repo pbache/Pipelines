43f209162af1d19d343f696e55cc66b625732a63
pbache <34801407+pbache@users.noreply.github.com>
2017-12-25 14:49:42 -0800

    Update Common.groovy

--- Common.groovy
+++ Common.groovy
@@ -1,3 +1,22 @@
+//def me =sh (script:'git whatchanged -n 1 --pretty=format: --name-only',returnStdout:true).trim().split('/')
+//def pckg= me.length>1?me[1]:'.'
+//currentBuild.displayName = pckg+currentBuild.displayName
+/*if(pckg == 'ui-web'){
+  def pipeui=load 'pipeline/ui-web.groovy'
+  pipeui.testui()
+}
+else if(pckg == 'lambdas'){
+  def pipeserv=load 'pipeline/ui-web.groovy'
+  pipeserv.testservices()
+}
+*/
+
+
+
+
+
+
+
 def name = sh ( script: 'git show --name-only origin/master' , returnStdout:true).trim()/split('/')
 println("----------output-----------")
 for (i = 0; i < name.length; i++)
