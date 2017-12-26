827f451375c6280e49fd0a224b4eeb0ede064895
pbache <34801407+pbache@users.noreply.github.com>
2017-12-26 08:59:23 -0800

    Update Common.groovy
    test

--- Common.groovy
+++ Common.groovy
@@ -1,15 +1,17 @@
 def prep(){
-  def name = sh ( script: 'git whatchanged -n 1 --pretty=format: --name-only' , returnStdout:true).trim().split('/')
+  def name = sh ( script: 'git show --name-only origin/master' , returnStdout:true).trim().split('/')
   println("----------output-----------")
   for (i = 0; i < name.length; i++){
     println(name[i])
   if(name[i] == 'ui-web'){
     def pipeui=load 'Pipelines/ui-web.groovy'
     pipeui.testui()
+    currentBuild.displayName = name[i]+currentBuild.displayName
   }
   if(name[i] == 'lambdas'){
     def pipeserv=load 'Pipelines/lambdas.groovy'
     pipeserv.testservice()
+    currentBuild.displayName = name[i]+currentBuild.displayName
   }
 }
 }
