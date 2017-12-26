b51634a785d457077ce0c0c332805815556d2ffc
pbache <34801407+pbache@users.noreply.github.com>
2017-12-25 21:07:54 -0800

    Update Common.groovy
    second

--- Common.groovy
+++ Common.groovy
@@ -1,3 +1,4 @@
+def prep(){
 //def me =sh (script:'git whatchanged -n 1 --pretty=format: --name-only',returnStdout:true).trim().split('/')
 //def pckg= me.length>1?me[1]:'.'
 //currentBuild.displayName = pckg+currentBuild.displayName
@@ -12,20 +13,20 @@ else if(pckg == 'lambdas'){
 */
 
 
-def name = sh ( script: 'git show --name-only origin/master' , returnStdout:true).trim().split('/')
-println("----------output-----------")
-for (i = 0; i < name.length; i++)
-  printlln(name[i])
-if(name[i] == 'ui-web'){
-  def pipeui=load 'pipeline/ui-web.groovy'
-  pipeui.testui()
-}
-else if(name[i] == 'lambdas'){
-  def pipeserv=load 'pipeline/ui-web.groovy'
-  pipeserv.testservices()
-}
+  def name = sh ( script: 'git show --name-only origin/master' , returnStdout:true).trim().split('/')
+  println("----------output-----------")
+  for (i = 0; i < name.length; i++)
+    printlln(name[i])
+  if(name[i] == 'ui-web'){
+    def pipeui=load 'ui-web.groovy'
+    pipeui.testui()
+  }
+  else if(name[i] == 'lambdas'){
+    def pipeserv=load 'lambdas.groovy'
+    pipeserv.testservices()
+  }
 
-
+}
 
 
 /*node{
