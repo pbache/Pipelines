16105adf635444eb2bd71b76e08f8a78a2c9c143
pbache <34801407+pbache@users.noreply.github.com>
2017-12-29 21:45:45 +0530

    Update Common.groovy
    removed pipeline steps

--- Common.groovy
+++ Common.groovy
@@ -33,16 +33,6 @@ return this
 */
 setup() {
 }
-def pipeline
- node ('master') {
-  checkout scm
-  def buildenv = "${params.DEPLOY_ENV}"
-  sh 'rm -fR Pipelines'
-  sh 'git clone https://github.com/pbache/Pipelines.git'
-  def commonpipe = load 'Pipelines/Common.groovy'
-  commonpipe.prep_common("${buildenv}")
-}
-
 
 def start() {
 
