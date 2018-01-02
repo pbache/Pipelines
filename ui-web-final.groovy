cbb50a824c53fd04b0bc93c9614f1b9893ecfaf7
pbache <mallikarjuna.pinnamraju@qentelli.com>
2018-01-02 15:43:49 +0530

    Updated ui-web-final.groovy

--- ui-web-final.groovy
+++ ui-web-final.groovy
@@ -1,6 +1,44 @@
 	def start()
 	{
 	
+	for (int i = 0; i < ui_stages.length ; i++) {
+        println i 
+        //def value = i
+        if (ui_stages[i] == "Prep") {
+            Prep()
+        }
+        
+        else if (ui_stages[i] == "unitTests"){
+        unitTests()
+        }
+        
+        else if (ui_stages[i] == "geminiTests")
+        {
+        geminiTests()
+        }
+        
+        else if (ui_stages[i] == "buildPublish")
+        {
+        buildPublish()
+        }
+        else if (ui_stages[i] == "versionChangelogTag")
+        {
+        versionChangelogTag()
+        }
+        
+        else if (ui_stages[i] == "deploy")
+        {
+        deploySTG()
+        }
+        else if (ui_stages[i] == "post")
+        {
+        post()
+        }
+        
+        else println "stage is not available"
+        
+    }
+	
 	}
 
 	def Prep()
